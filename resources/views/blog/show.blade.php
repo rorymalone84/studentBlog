8cf08f2c962006d6b8eadf79d9ad38a544c07e7c
rory <rorymalone@live.com>
2021-11-30 01:34:15 +0000

    show post display complete

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -11,7 +11,16 @@
 </div>
 
 <div class="w-4/5 m-auto pt-20">
+    <span class="text-gray-500">
+        By: <span class="font-bold italic text-gray-800">{{$post->user->name}}</span>
+    </span>
+    <span class="text-gray-500 pt-20">
+       -  Posted: {{$post->created_at->diffForHumans()}}
+    </span>
     
+    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+        {!! $post->description !!}
+    </p>
 </div>
     
 @endsection
