2f77ff6bd09aa517ce0d6d7962b31311bccdb3ca
rory <rorymalone@live.com>
2022-02-16 12:55:15 +0000

    bucket issue test

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,7 +47,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION','eu-west-2'),
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => env('AWS_BUCKET','martins-blog-bucket'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
